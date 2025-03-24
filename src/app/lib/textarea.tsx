af0f7c4c71f1f698b38c0b08406844099e241be5
Alfonsus Ardani <alfonsusac@gmail.com>
2025-03-24 13:05:43 +0700

    added textarea

---
+++ src/app/lib/textarea.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, type Component, type ComponentProps } from "react";
+
+export function TextArea(props: ComponentProps<'textarea'>) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const adjustHeight = () => {
+    const textarea = textareaRef.current;
+    if (!textarea) return;
+    textarea.style.height = "auto"; // Reset height
+    textarea.style.height = `${ textarea.scrollHeight }px`; // Set to scroll height
+  };
+
+  useEffect(() => {
+    adjustHeight(); // Adjust height on mount (for initial value)
+  }, []);
+
+
+  return <textarea {...props}
+    onChange={(e) => {
+      adjustHeight();
+      props.onChange?.(e)
+    }}
+    ref={(ref) => {
+      if (ref) {
+        textareaRef.current = ref
+        const outerRef = props.ref
+        if (typeof outerRef === 'function') outerRef(ref)
+        else if (outerRef) outerRef.current = ref
+      }
+    }} />;
+}
